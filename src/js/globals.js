dcd6a47a4fa6e5783f36d9d54cd93ce3a67f51a9
ytr0221 <ytr0@ymail.ne.jp>
2022-07-31 06:40:39 +0900

    added note

--- src/js/globals.js
+++ src/js/globals.js
@@ -6,7 +6,7 @@ export const STATIC_INDEX = "99998";
 
 export const DEFAULT_MEMO = {
   id: "9346dc6c-f982-48ce-9e21-f6a334b367b3",
-  text: "Manifest is a grid-based pinboard for note taking.\n\nSimply click and drag anywhere to create a memo and snap it to the grid. All memos can be moved, resized, deleted and are saved in your browser's local storage between sessions.\n\nTo toggle between dark and light modes you can press Alt + T.\n\nManifest is free and completely open-source. If you find a bug or have a suggestion feel free to file an issue on Github.\n\nhttps://github.com/jonathontoon/manifest/issues",
+  text: "GRID is a task management system with notes on a pinboard. It looks like a file manager. \n\n Press [N] key to create a new note. \n\n  Press [X] key to delete a note. \n\n \n\n All memos are saved in your browser's local storage between sessions.\n\nTo toggle between dark and light modes you can press Alt + T.\n\nManifest is free and completely open-source. If you find a bug or have a suggestion feel free to file an issue on Github.\n\nhttps://github.com/jonathontoon/manifest/issues",
   position: {
     top: 30,
     left: 750
