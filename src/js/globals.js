9eee84d44a04a254981823b1ddf6eb84554eacf8
ytr0221 <ytr0@ymail.ne.jp>
2022-07-31 03:10:37 +0900

    test

--- src/js/globals.js
+++ src/js/globals.js
@@ -8,8 +8,8 @@ export const DEFAULT_MEMO = {
   id: "9346dc6c-f982-48ce-9e21-f6a334b367b3",
   text: "Manifest is a grid-based pinboard for note taking.\n\nSimply click and drag anywhere to create a memo and snap it to the grid. All memos can be moved, resized, deleted and are saved in your browser's local storage between sessions.\n\nTo toggle between dark and light modes you can press Alt + T.\n\nManifest is free and completely open-source. If you find a bug or have a suggestion feel free to file an issue on Github.\n\nhttps://github.com/jonathontoon/manifest/issues",
   position: {
-    top: 50,
-    left: 50
+    top: 30,
+    left: 750
   },
   size: {
     width: 379,
